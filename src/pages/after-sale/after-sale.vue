2a4c109a99a859b781b15508bdba3f2eaa824f2d
kylong <1603049109@qq.com>
2018-12-21 12:21:09 +0800

    k

--- src/pages/after-sale/after-sale.vue
+++ src/pages/after-sale/after-sale.vue
@@ -1,98 +1,108 @@
 <template>
-    <div class="sale-wallper">
-        <div class="content">
-          <Scroll ref="scroll" :pullUpLoad="null">
-            <div class="sale-head">
-                <div class="sale-jtk" @click="setSaleFn('0')">
-                    <div class="jtk-left sale-left">
-                        <p :style="{'background-image': setIndex == '0' ? `url('/static/yxz.png')` : ''}"></p>
-                    </div>
-                    <div class="jtk-right sale-right">
-                        <h5>仅退款</h5>
-                        <p>卖家未发货或与卖家协商同意前提下</p>
-                    </div>
-                </div>
-                <div class="sale-thtk" @click="setSaleFn('1')">
-                    <div class="thtk-left sale-left">
-                        <p :style="{'background-image': setIndex == '1' ? `url('/static/yxz.png')` : ''}"></p>
-                    </div>
-                    <div class="thtk-right sale-right">
-                        <h5>退货退款</h5>
-                        <p>卖家已发货，需要退货物</p>
-                    </div>
-                </div>
-            </div>
-            <p class="back-co" v-show="isset"></p>
-            <div class="sale-con" v-show="isset">
-                <div class="sale-box01">
-                    <div class="box-left">
-                        <p>退款金额</p>
-                    </div>
-                    <div class="box-right">
-                      <input name="money" v-validate="'required'" type="text" placeholder="请输入金额" v-model="refundAmount">
-                      <span v-show="errors.has('money')" class="error-tip">{{errors.first('money')}}</span>
-                    </div>
-                </div>
-                <div class="sale-box01" v-show="setIndex === '1'">
-                    <div class="box-left">
-                        <p>物流公司</p>
-                    </div>
-                    <div class="box-right">
-                      <input name="company" v-validate="'required'" type="text" placeholder="请输入物流公司" v-model="salesConfig.logisticsCompany">
-                      <span v-show="errors.has('company')" class="error-tip">{{errors.first('company')}}</span>
-                    </div>
-                </div>
-                <div class="sale-box01" v-show="setIndex === '1'">
-                    <div class="box-left">
-                        <p>物流单号</p>
-                    </div>
-                    <div class="box-right">
-                      <input name="logistics" v-validate="'required'" type="text" placeholder="请输入物流单号" v-model="salesConfig.logisticsNumber">
-                      <span v-show="errors.has('logistics')" class="error-tip">{{errors.first('logistics')}}</span>
-                    </div>
-                </div>
-                <div class="sale-box01" v-show="setIndex === '1'">
-                    <div class="box-left">
-                        <p>发货人</p>
-                    </div>
-                    <div class="box-right">
-                      <input name="deliver" v-validate="'required'" type="text" placeholder="请输入发货人" v-model="salesConfig.deliver">
-                      <span v-show="errors.has('deliver')" class="error-tip">{{errors.first('deliver')}}</span>
-                    </div>
-                </div>
-              <div class="sale-box01">
-                <div class="box-left">
-                  <p>退款原因</p>
-                </div>
-                <div class="box-right">
-                  <textarea name="why" type="text" placeholder="请输入退款原因(选填)" v-model="refundReason"></textarea>
-                </div>
-              </div>
-              <div class="sale-box01">
-                <div class="box-left">
-                  <p>留言</p>
-                </div>
-                <div class="box-right">
-                  <textarea name="why" type="text" placeholder="欢迎留言`(选填)" v-model="message"></textarea>
-                </div>
-              </div>
-            </div>
-            <div class="sale-foo" v-show="isset" @click="afterSale">
-                提交
-            </div>
-          </Scroll>
+<div class="sale-wallper">
+  <div class="content">
+    <Scroll ref="scroll" :pullUpLoad="null">
+      <div class="sale-head">
+        <div class="sale-jtk" @click="setSaleFn('0')">
+          <div class="jtk-left sale-left">
+            <p :style="{'background-image': setIndex == '0' ? `url('/static/yxz.png')` : ''}"></p>
+          </div>
+          <div class="jtk-right sale-right">
+            <h5>仅退款</h5>
+            <p>卖家未发货或与卖家协商同意前提下</p>
+          </div>
         </div>
-        <full-loading v-show="loading" :title="loadingText"></full-loading>
-        <toast ref="toast" :text="textMsg"></toast>
-    </div>
+        <div class="sale-thtk" @click="setSaleFn('1')">
+          <div class="thtk-left sale-left">
+            <p :style="{'background-image': setIndex == '1' ? `url('/static/yxz.png')` : ''}"></p>
+          </div>
+          <div class="thtk-right sale-right">
+            <h5>退货退款</h5>
+            <p>卖家已发货，需要退货物</p>
+          </div>
+        </div>
+      </div>
+      <p class="back-co" v-show="isset"></p>
+      <div class="sale-con" v-show="isset">
+        <div class="sale-box01">
+          <div class="box-left">
+            <p>退款金额</p>
+          </div>
+          <div class="box-right">
+            <input name="money" v-validate="'required'" type="text" placeholder="请输入金额" v-model="refundAmount">
+            <span v-show="errors.has('money')" class="error-tip">{{errors.first('money')}}</span>
+          </div>
+        </div>
+        <div class="sale-box01" v-show="setIndex === '1'">
+          <div class="box-left">
+            <p>物流公司</p>
+          </div>
+          <div class="box-right">
+            <input name="company" v-validate="'required'" type="text" placeholder="请输入物流公司" v-model="salesConfig.logisticsCompany">
+            <span v-show="errors.has('company')" class="error-tip">{{errors.first('company')}}</span>
+          </div>
+        </div>
+        <div class="sale-box01" v-show="setIndex === '1'">
+          <div class="box-left">
+            <p>物流单号</p>
+          </div>
+          <div class="box-right">
+            <input name="logistics" v-validate="'required'" type="text" placeholder="请输入物流单号" v-model="salesConfig.logisticsNumber">
+            <span v-show="errors.has('logistics')" class="error-tip">{{errors.first('logistics')}}</span>
+          </div>
+        </div>
+        <div class="sale-box01" v-show="setIndex === '1'">
+          <div class="box-left">
+            <p>发货人</p>
+          </div>
+          <div class="box-right">
+            <input name="deliver" v-validate="'required'" type="text" placeholder="请输入发货人" v-model="salesConfig.deliver">
+            <span v-show="errors.has('deliver')" class="error-tip">{{errors.first('deliver')}}</span>
+          </div>
+        </div>
+        <div class="sale-box01">
+          <div class="box-left">
+            <p>退款原因</p>
+          </div>
+          <div class="box-right">
+            <textarea name="why" type="text" placeholder="请输入退款原因(选填)" v-model="refundReason"></textarea>
+          </div>
+        </div>
+        <div class="sale-box01">
+          <div class="box-left">
+            <p>留言</p>
+          </div>
+          <div class="box-right">
+            <textarea name="why" type="text" placeholder="欢迎留言(选填)" v-model="message"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="sale-foo" v-show="isset" @click="afterSale">
+        提交
+      </div>
+    </Scroll>
+  </div>
+  <full-loading v-show="loading" :title="loadingText"></full-loading>
+  <toast ref="toast" :text="textMsg"></toast>
+</div>
 </template>
 
 <script>
 import FullLoading from 'base/full-loading/full-loading';
 import Scroll from 'base/scroll/scroll';
 import Toast from 'base/toast/toast';
-import { formatAmount, formatImg, formatDate, setTitle, getUserId } from 'common/js/util';
-import { refundMoney, salesReturn, oneStoreOrder } from 'api/store';
+import {
+  formatAmount,
+  formatImg,
+  formatDate,
+  setTitle,
+  getUserId
+} from 'common/js/util';
+import {
+  refundMoney,
+  salesReturn,
+  oneStoreOrder
+} from 'api/store';
 export default {
   data() {
     return {
@@ -102,15 +112,15 @@ export default {
       textMsg: '',
       loadingText: '正在加载中...',
       salesConfig: { // 退货参数
-        orderDetailCode: '',   // 订单明细编号
-        logisticsCompany: '',    // 物流公司
-        logisticsNumber: '',    // 物流单号
-        refundAmount: '',    // 退款金额
-        deliver: '',    // 发货人
+        orderDetailCode: '', // 订单明细编号
+        logisticsCompany: '', // 物流公司
+        logisticsNumber: '', // 物流单号
+        refundAmount: '', // 退款金额
+        deliver: '', // 发货人
         refundReason: '',
         message: ''
       },
-      refundConfig: {  // 退款入参
+      refundConfig: { // 退款入参
         orderDetailCode: '',
         refundAmount: '',
         refundReason: '',
@@ -161,7 +171,7 @@ export default {
     setSaleFn(index) {
       this.refundReason = '';
       this.message = '';
-      if(index === this.setIndex) {
+      if (index === this.setIndex) {
         this.setIndex = '';
         this.isset = false;
         return;
@@ -170,9 +180,9 @@ export default {
       this.isset = true;
     },
     afterSale() {
-      switch(this.setIndex) {
+      switch (this.setIndex) {
         case '0': // 退款
-          if(this.refundAmount === '') {
+          if (this.refundAmount === '') {
             this.textMsg = '请填写完整';
             this.$refs.toast.show();
             return;
@@ -195,7 +205,7 @@ export default {
           });
           break;
         case '1': // 退货
-          if(this.refundAmount === '' || this.salesConfig.logisticsCompany === '' || this.salesConfig.logisticsNumber === '') {
+          if (this.refundAmount === '' || this.salesConfig.logisticsCompany === '' || this.salesConfig.logisticsNumber === '') {
             this.textMsg = '请填写完整';
             this.$refs.toast.show();
             return;
@@ -231,112 +241,110 @@ export default {
 @import "../../common/scss/mixin.scss";
 @import "../../common/scss/variable.scss";
 .sale-wallper {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  .fl {
-    float: left;
-  }
-  .fr {
-    float: right;
-  }
-  .error-tip{
-    color: red;
-  }
-  .content {
-    position: absolute;
+    position: fixed;
     top: 0;
-    bottom: 0;
     left: 0;
-    right: 0;
-    padding-bottom: 0.6rem;
-    overflow: auto;
-    background-color: #fff;
-    font-family: PingFangSC-Regular;
-    .back-co{
-        height: 0.2rem;
-        background-color: #f5f5f5;
+    bottom: 0;
+    width: 100%;
+    .fl {
+        float: left;
     }
-    .sale-head{
-      padding: 0 0.3rem;
-        >div{
-            height: 1.8rem;
-            padding: 0.3rem 0;
-            display: flex;
-            background: rgba(216,216,216,0.00);
-            box-shadow: 0 0.02rem 0 0 #EBEBEB;
-            font-family: PingFang-SC-Medium;
-            .sale-left{
-                width: 0.66rem;
-                padding-top: 0.36rem;
-                box-sizing: border-box;
-                p{
-                    width: 0.36rem;
-                    height: 0.36rem;
-                    border-radius: 100%;
-                    background-size: 100% 100%;
-                    border: 0.02rem solid #CCCCCC;
-                }
-            }
-            .sale-right{
-                width: 100%;
+    .fr {
+        float: right;
+    }
+    .error-tip {
+        color: red;
+    }
+    .content {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding-bottom: 0.6rem;
+        overflow: auto;
+        background-color: #fff;
+        font-family: PingFangSC-Regular;
+        .back-co {
+            height: 0.2rem;
+            background-color: #f5f5f5;
+        }
+        .sale-head {
+            padding: 0 0.3rem;
+            > div {
+                height: 1.8rem;
+                padding: 0.3rem 0;
                 display: flex;
-                flex-wrap: wrap;
-                align-content: space-around;
-                h5{
-                    width: 100%;
-                    font-size: 0.32rem;
-                    color: #333333;
-                    letter-spacing: 0.0025rem;
+                background: rgba(216,216,216,0.00);
+                box-shadow: 0 0.02rem 0 0 #EBEBEB;
+                font-family: PingFang-SC-Medium;
+                .sale-left {
+                    width: 0.66rem;
+                    padding-top: 0.36rem;
+                    box-sizing: border-box;
+                    p {
+                        width: 0.36rem;
+                        height: 0.36rem;
+                        border-radius: 100%;
+                        background-size: 100% 100%;
+                        border: 0.02rem solid #CCCCCC;
+                    }
                 }
-                p{
+                .sale-right {
                     width: 100%;
-                    font-size: 0.26rem;
-                    color: #999999;
-                    letter-spacing: 0.0023rem;
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-content: space-around;
+                    h5 {
+                        width: 100%;
+                        font-size: 0.32rem;
+                        color: #333333;
+                        letter-spacing: 0.0025rem;
+                    }
+                    p {
+                        width: 100%;
+                        font-size: 0.26rem;
+                        color: #999999;
+                        letter-spacing: 0.0023rem;
+                    }
                 }
             }
-        }
 
-    }
-    .sale-con{
-      padding: 0 0.3rem;
-        >div{
-            padding: 0.38rem 0;
-            font-family: PingFang-SC-Medium;
-            letter-spacing: 0.0022rem;
-            font-size: 0.28rem;
-            color: #333333;
-            display: flex;
-            background: rgba(216,216,216,0.00);
-            box-shadow: 0 0.02rem 0 0 #EBEBEB;
-            .box-left{
-                width: 30%;
-            }
-            .box-right{
-                width: 70%;
-              textarea{
-                height: 1.2rem;
-                line-height: 1.2;
-                width: 100%;
-              }
+        }
+        .sale-con {
+            padding: 0 0.3rem;
+            > div {
+                padding: 0.38rem 0;
+                font-family: PingFang-SC-Medium;
+                letter-spacing: 0.0022rem;
+                font-size: 0.28rem;
+                color: #333333;
+                display: flex;
+                background: rgba(216,216,216,0.00);
+                box-shadow: 0 0.02rem 0 0 #EBEBEB;
+                .box-left {
+                    width: 30%;
+                }
+                .box-right {
+                    width: 70%;
+                    textarea {
+                        height: 1.2rem;
+                        line-height: 1.2;
+                        width: 100%;
+                    }
+                }
             }
         }
+        .sale-foo {
+            margin: 0.6rem 0.3rem 0;
+            height: 0.9rem;
+            line-height: 0.9rem;
+            text-align: center;
+            font-size: 0.36rem;
+            color: #fff;
+            background-color: #23AD8C;
+            border-radius: 0.12rem;
+        }
     }
-    .sale-foo{
-      margin: 0 0.3rem;
-      margin-top: 0.6rem;
-      height: 0.9rem;
-      line-height: 0.9rem;
-      text-align: center;
-      font-size: 0.36rem;
-      color: #fff;
-      background-color: #23AD8C;
-      border-radius: 0.12rem;
-    }
-  }
 }
 </style>
-
